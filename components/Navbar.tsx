eea6d390c26a9c4356acac183b826dce46041e3a
trefferrr <trefferx07@gmail.com>
2024-01-25 14:24:31 +0530

    components commit 2

---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="yoom logo"
+          className="mr-5 max-sm:size-10"
+        />
+        <p className="text-[32px]  text-white max-sm:hidden" style={{ fontFamily: 'Apple Chancery' }}>
+  Connect..
+</p>
+  </Link>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
